93327fe9e7ca405d8d38328ef37932b4e479c545
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-09-17 12:42:41 +0200

    feat: basic Rock paper sissors implementation

--- Boids.js
+++ Boids.js
@@ -1,25 +1,47 @@
+const Types = {
+  Rock: 0,
+  Paper: 1,
+  Scissors: 2,
+};
+
 class Boid {
   static SEPARATION_RADIUS = 30;
   static VIEW_RADIUS = 100;
+  static COLLISION_RADIUS = 10;
   static SPEED = 2;
 
-  static SEPARATION_WEIGHT = 0.06;
-  static ALIGNMENT_WEIGHT = 0.05;
+  static SEPARATION_WEIGHT = 0.05;
+  static ALIGNMENT_WEIGHT = 0.01;
   static COHESION_WEIGHT = 0.04;
+
+  static SHOW_RADIA = false;
+
+  static Emojis = ["🗿", "📄", "✂️"];
 
   constructor(
     position,
-    velocity = createVector(1, 0).setHeading(random(TWO_PI))
+    velocity = createVector(1, 0).setHeading(random(TWO_PI)),
+    Type = floor(random(3))
   ) {
     this.position = position;
     this.velocity = velocity;
+    this.Type = Type;
   }
 
   draw() {
     push();
+    fill(255, 255);
     translate(this.position.x, this.position.y);
-    rotate(this.velocity.heading());
-    triangle(0, -5, 0, 5, 15, 0);
+    rotate(this.velocity.heading() + PI / 2);
+    //triangle(0, -5, 0, 5, 15, 0);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text(Boid.Emojis[this.Type], 0, 0);
+
+    if (!Boid.SHOW_RADIA) {
+      pop();
+      return;
+    }
 
     // Draw view radius
     noFill();
@@ -30,7 +52,19 @@ class Boid {
     stroke(255, 0, 0);
     circle(0, 0, Boid.SEPARATION_RADIUS * 2);
 
+    // Draw collision radius
+    stroke(0, 255, 0);
+    circle(0, 0, Boid.COLLISION_RADIUS * 2);
+
     pop();
+  }
+
+  isWeakToMe(other) {
+    return (this.Type + 2) % 3 == other.Type;
+  }
+
+  isStrongToMe(other) {
+    return (this.Type + 1) % 3 == other.Type;
   }
 
   updatePosition() {
@@ -43,6 +77,7 @@ class Boid {
     let avoidance = createVector(0, 0);
     boids.forEach((boid) => {
       if (
+        !this.isWeakToMe(boid) &&
         p5.Vector.dist(this.position, boid.position) < Boid.SEPARATION_RADIUS
       ) {
         let diff = p5.Vector.sub(this.position, boid.position);
@@ -58,7 +93,10 @@ class Boid {
     let alignment = createVector(0, 0);
     let n = 0;
     boids.forEach((boid) => {
-      if (p5.Vector.dist(this.position, boid.position) < Boid.VIEW_RADIUS) {
+      if (
+        this.Type == boid.Type &&
+        p5.Vector.dist(this.position, boid.position) < Boid.VIEW_RADIUS
+      ) {
         alignment.add(boid.velocity);
         n++;
       }
@@ -73,7 +111,10 @@ class Boid {
     let centerOfMass = createVector(0, 0);
     let n = 0;
     boids.forEach((boid) => {
-      if (p5.Vector.dist(this.position, boid.position) < Boid.VIEW_RADIUS) {
+      if (
+        !this.isStrongToMe(boid) &&
+        p5.Vector.dist(this.position, boid.position) < Boid.VIEW_RADIUS
+      ) {
         centerOfMass.add(boid.position);
         n++;
       }
@@ -91,5 +132,16 @@ class Boid {
     this.velocity.add(this.computeAlignment(boids));
     this.velocity.add(this.computeCohesion(boids));
     this.velocity.normalize();
+  }
+
+  checkCollisions(boids) {
+    boids.forEach((boid) => {
+      if (
+        this.isStrongToMe(boid) &&
+        p5.Vector.dist(this.position, boid.position) < Boid.COLLISION_RADIUS * 2
+      ) {
+        this.Type = boid.Type;
+      }
+    });
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,45 @@
-const N_BOIDS = 10;
+const N_BOIDS = 80;
 let Boids = [];
+
+function drawPieChart() {
+  let rocks = 0;
+  let papers = 0;
+  let scissors = 0;
+  let total = 0;
+  Boids.forEach((boid) => {
+    if (boid.Type == 0) {
+      rocks++;
+    }
+    if (boid.Type == 1) {
+      papers++;
+    }
+    if (boid.Type == 2) {
+      scissors++;
+    }
+    total++;
+  });
+  let rockPerc = rocks / total;
+  let paperPerc = papers / total;
+  let scissorsPerc = scissors / total;
+
+  let radius = 100;
+  let aplha = 100;
+  let x = width - radius;
+  let y = height - radius;
+  let lastAngle = 0;
+  let rockAngle = rockPerc * 2 * PI;
+  let paperAngle = paperPerc * 2 * PI;
+  let scissorsAngle = scissorsPerc * 2 * PI;
+  fill(200, 200, 200, aplha);
+  arc(x, y, radius, radius, lastAngle, lastAngle + rockAngle);
+  lastAngle += rockAngle;
+  fill(243, 238, 248, aplha);
+  arc(x, y, radius, radius, lastAngle, lastAngle + paperAngle);
+  lastAngle += paperAngle;
+  fill(243, 48, 46, aplha);
+  arc(x, y, radius, radius, lastAngle, lastAngle + scissorsAngle);
+  lastAngle += scissorsAngle;
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,6 +54,7 @@ function draw() {
 
   Boids.forEach((boid) => boid.updateVelocity(Boids));
   Boids.forEach((boid) => boid.updatePosition());
-
+  Boids.forEach((boid) => boid.checkCollisions(Boids));
+  drawPieChart();
   Boids.forEach((boid) => boid.draw());
 }
